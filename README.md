3cb61472d051b446cb8fbbe7ac95526d390c5938
Todd Francis <todd@toddish.co.uk>
2013-01-22 23:04:28 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Filtr.js
+
+---
+
+A small jQuery plugin that helps filter a selection of items.
+
+---
+
+## Features
+
+* Can filter any selection of elements...
+* On any property you want!
+* Lightweight - ~0.5kb minified and gzipped
+* Made to be customized - callbacks and options
+
+---
+
+## Installation
+
+Place `/assets/js/filtr.min.js` in `/your/js/folder/`.
+
+Add this just before your closing `body` tag, after you've included jQuery:
+
+```html
+<script src="/your/js/folder/filtr.min.js"></script>
+```
+
+---
+
+## Usage
+
+Set up your html:
+
+```html
+<form action="/">
+    <input name="filter">
+</form>
+
+<ul class="filter_items">
+    <li data-filtr="item 1">Item 1</li>
+    <li data-filtr="item 2">Item 2</li>
+    <li data-filtr="item 3">Item 3</li>
+</ul>
+```
+
+Call the plugin:
+
+```javascript
+var options = {};
+$('input[name="filter"]').filter($('.filter_items li'), options);
+```
+
+---
+
+## Documentation
+
+For full documentation, have a look at [http://docs.toddish.co.uk/filtr](http://docs.toddish.co.uk/filtr).
---
+++ assets/js/jquery.filtr.js
@@ -0,0 +1,148 @@
+/*----------------------------------------------
+
+	FILTR.JS
+
+	Filter a selection of elements
+
+	@author Todd Francis
+	@version 1.0.0
+
+----------------------------------------------*/
+
+;(function($, window){
+
+	'use strict';
+
+	/**
+	 * Filters a list
+	 *
+	 * @param  {Object} $items  jQuery selection object
+	 * @param  {Object} settings
+	 * @return {Object}         Initial jQuery selection object
+	 */
+	$.fn.filtr = function($items, settings){
+
+		var defaults = {
+				beforeFilter		: function(value){ return value; },
+				afterFilter			: function(){},
+				beforeManip			: function(toShow, toHide){},
+				show				: function($item){
+					$item.show();
+				},
+				hide				: function($item){
+					$item.hide();
+				},
+				checkItem			: function(value, item){
+					var reg = new RegExp(value.split('').join('[\\w\\W]*'), 'i');
+					return item.data.match(reg);
+				},
+				trigger				: 'keyup',
+				wait				: 200,
+				attr				: 'data-filtr'
+			},
+			o = $.extend(true, {}, defaults, settings),
+			p = this,
+			toShow = [],
+			toHide = [],
+			timeout;
+
+		// Loop through each item
+		return this
+			.each(function(){
+
+				var $input = $(this),
+					items = [],
+					bind = [];
+
+				// Populate the item
+				$items
+					.each(function(){
+
+						var $item = $(this);
+
+						// Build the array
+						items.push({
+							data	: $item.attr(o.attr),
+							item	: $item
+						});
+
+					});
+
+				bind[o.trigger] = function(){
+
+					// Clear timeout if there currently is one set
+					if( timeout ){
+						window.clearTimeout(timeout);
+					}
+
+					// Set the new timeout
+					timeout = window.setTimeout(function() {
+
+						var val = $input.val();
+
+						// Call the before callback
+						val = o.beforeFilter.call(p, val);
+
+						// Filter the items
+						filter(val, items);
+
+						// Call the after callback
+						o.afterFilter.call(p);
+
+					}, o.wait);
+
+				};
+
+				// Bind the event
+				$input.on(bind);
+
+			});
+
+		/**
+		 * Filter the list
+		 *
+		 * @param  {string} value	value to filter by
+		 * @param  {array} items	items to filter against
+		 * @return {void}
+		 */
+		function filter(value, items) {
+
+			// Clear
+			toShow.length = 0;
+			toHide.length = 0;
+
+			// Filter the items
+			items.filter(function(item) {
+
+				if( o.checkItem.call(p, value, item) ){
+
+					toShow.push(item.item);
+
+				}else{
+
+					toHide.push(item.item);
+
+				}
+
+			});
+
+			o.beforeManip.call(p, toShow, toHide);
+
+			// Show the ones that need to be shown
+			$.each(toShow, function(){
+
+				o.show.call(p, $(this));
+
+			});
+
+			// Hide the ones that need to be hidden
+			$.each(toHide, function(){
+
+				o.hide.call(p, $(this));
+
+			});
+		}
+
+	};
+
+}(jQuery, window));
